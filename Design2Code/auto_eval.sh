87019c193823c9aee55ba7b7287869a17a2f7c6e
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-09 15:57:58 +0000

    eval new models

--- Design2Code/auto_eval.sh
+++ Design2Code/auto_eval.sh
@@ -3,10 +3,28 @@
 # 定义一个数组，包含所有需要处理的模型名称  
 model_names=(  
     # "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job"
-    "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_1000"
-    "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_1500"  
+    # "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_1000"
+    # "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_1500"  
+    # "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_2000"
+    # "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_3000"
     "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_2000"
     "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_3000"
+    "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_4000"
+    "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_5000"
+    "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_6000"    
+    "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_7000"
+    "qwen2_vl_coder_mn_pretrain_LLM_sft-7b_3072_bsz128_1e4_web2code_job_8000"
+
+    "qwen2_vl_coder_mn_only-7b_3072_bsz128_1e4_web2code_merger_500"
+    "qwen2_vl_coder_mn_only-7b_3072_bsz128_1e4_web2code_merger_1000"
+    "qwen2_vl_coder_mn_only-7b_3072_bsz128_1e4_web2code_merger_1500"
+    "qwen2_vl_coder_mn_only-7b_3072_bsz128_1e4_web2code_merger_2000"
+    "qwen2_vl_coder_mn_only-7b_3072_bsz128_1e4_web2code_merger_3000"
+    "qwen2_vl_coder_mn_only-7b_3072_bsz128_1e4_web2code_merger_4000"
+    "qwen2_vl_coder_mn_only-7b_3072_bsz128_1e4_web2code_merger_5000"
+    "qwen2_vl_coder_mn_only-7b_3072_bsz128_1e4_web2code_merger_6000"
+    "qwen2_vl_coder_mn_only-7b_3072_bsz128_1e4_web2code_merger_7000"
+    "qwen2_vl_coder_mn_only-7b_3072_bsz128_1e4_web2code_merger_8000"
     # "another_model_name_part1_part2" # 可以添加其他模型名称  
 )  
   
