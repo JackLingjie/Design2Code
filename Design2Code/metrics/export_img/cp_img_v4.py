db67be24c5684200da5d6c0235c42bd4f3aa3cda
lingjiejiang <lingjiejiang@stu.pku.edu.cn>
2025-01-18 08:21:07 +0000

    eavl

---
+++ Design2Code/metrics/export_img/cp_img_v4.py
@@ -0,0 +1,37 @@
+import json  
+import os  
+import shutil  
+import random  
+  
+# Define paths  
+image_output_dir = "/mnt/lingjiejiang/multimodal_code/data/github_html_v4/images/"  
+image_preview_dir = "image_preview/"  # Update with the actual path  
+  
+# Ensure the image_preview directory exists  
+os.makedirs(image_preview_dir, exist_ok=True)  
+  
+# Load data from the file  
+# html_data_path = "/mnt/unilm/shaohanh/data/code/purehtml_v2.txt"  
+html_data_path = "/mnt/unilm/shaohanh/data/code/purehtml_v4/merged.jsonl"
+
+with open(html_data_path, "r", encoding="utf-8") as f:  
+    lines = f.readlines()  
+    data = [json.loads(line) for line in lines]  
+  
+# Randomly sample 50 items  
+sampled_data = random.sample(data, 10)  
+  
+# Function to copy images  
+def copy_image_to_preview(item):  
+    item_id = item.get('id')  
+    predict_img_path = os.path.join(image_output_dir, f"{item_id}.png")  
+  
+    # Copy existing image to image_preview directory if it exists  
+    if os.path.exists(predict_img_path):  
+        shutil.copy(predict_img_path, image_preview_dir)  
+  
+# Copy images for sampled data  
+for item in sampled_data:  
+    copy_image_to_preview(item)  
+  
+print("Copied images for 50 sampled items to the image_preview directory.")  
